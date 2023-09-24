95de342e7a7ea0990c5af5480cb696043dfc58fb
vkpvivek <vkpvivek67@gmail.com>
2023-09-24 18:02:17 +0530

    Login Api

--- Back_API/controllers/userAPI.js
+++ Back_API/controllers/userAPI.js
@@ -19,6 +19,38 @@ exports.postUser= async (req,res,next)=>{
 };
 
 
+exports.userLogin= async (req,res,next)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+
+    const obj= await User.findAll({
+        where:{
+            email:email
+        }
+    });
+
+
+    // if(obj.length>0){
+    //     //check if password matches
+    //     const pass=obj[0].password;
+    //     if(password==pass){
+    //         console.log("login successfully");
+    //     }else{
+    //         console.log("password didn't match");
+    //     }
+    // }
+    // else   //if user doesn't exist
+    // {
+    //     console.log("User Not Found");
+    // }
+
+    
+    res.status(201).json({
+        newUserDetails:obj
+    });
+};
+
+
 // exports.getUser= async (req,res,next)=>{
 //     const users= await User.findAll();
 
--- Back_API/routes/user.js
+++ Back_API/routes/user.js
@@ -5,6 +5,8 @@ const router=express.Router();
 
 router.post('/add-user',userController.postUser);
 
+router.post('/login',userController.userLogin);
+
 
 router.get('/home',(req,res,next)=>{
     res.send("HomePage");
