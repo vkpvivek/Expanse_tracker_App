d32347204cb8ce8646eb90df78949b54e6204959
vkpvivek <vkpvivek67@gmail.com>
2023-10-12 16:54:39 +0530

    leaderboard optimization 1

--- Back_API/controllers/premiumAPI.js
+++ Back_API/controllers/premiumAPI.js
@@ -6,34 +6,46 @@ exports.getLeaderBoard= async (req,res)=>{
 
     try{
 
-        const users=await User.findAll();
-        const expanses=await Expanse.findAll();
-        const userAggregatedExpanses ={}
+        // const users=await User.findAll();
+        // const expanses=await Expanse.findAll();
+        // const userAggregatedExpanses ={}
 
-        expanses.forEach((expanse)=>{
+        // expanses.forEach((expanse)=>{
 
-            if(userAggregatedExpanses[expanse.userId]){
-                userAggregatedExpanses[expanse.userId]=userAggregatedExpanses[expanse.userId]+ expanse.amount;
-            }else{
-                userAggregatedExpanses[expanse.userId]=expanse.amount;
-            }
+        //     if(userAggregatedExpanses[expanse.userId]){
+        //         userAggregatedExpanses[expanse.userId]=userAggregatedExpanses[expanse.userId]+ expanse.amount;
+        //     }else{
+        //         userAggregatedExpanses[expanse.userId]=expanse.amount;
+        //     }
 
+        // })
+
+        // console.log("---------------------------");
+        // var userleaderBoardDetails=[];
+
+        // users.forEach((user)=>{
+        //     userleaderBoardDetails.push({username:user.username, totalCost:userAggregatedExpanses[user.id] })
+        // })
+        // console.log(userleaderBoardDetails);
+        // //sorting
+        // userleaderBoardDetails.sort((a,b)=> b.totalCost -a.totalCost);
+        // res.status(200).json(userleaderBoardDetails);
+
+
+        const aggrLeaderBoard= await User.findAll({
+            attributes:['id','username',[sequelize.fn('sum',sequelize.col('expanses.amount')),'totalCost']],
+            include:[
+                {
+                    model:Expanse,
+                    attributes:[]
+                }
+            ],
+            group:['user.id'],
+            order:[['totalCost','DESC']]
+            //order:[sequelize.col('totalCost'),'DESC']
         })
 
-        console.log("---------------------------");
-        var userleaderBoardDetails=[];
-
-        users.forEach((user)=>{
-            userleaderBoardDetails.push({username:user.username, totalCost:userAggregatedExpanses[user.id] })
-        })
-        console.log(userleaderBoardDetails);
-
-        //sorting
-        userleaderBoardDetails.sort((a,b)=> b.totalCost -a.totalCost);
-
-
-        res.status(200).json(userleaderBoardDetails);
-        //res.status(200).json({message:"Testing"});
+        res.status(200).json(aggrLeaderBoard);
 
 
     }catch(err){
